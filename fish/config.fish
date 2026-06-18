19a28dc7d0ba3e0a3c024b28fb3b33ae46ea113e
Tu Nombre <tu_correo@ejemplo.com>
2026-06-18 20:46:10 +0000

    Fish command

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,7 @@ if status is-interactive
     # Agregar binarios locales al PATH
     fish_add_path $HOME/.local/bin
 end
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/vscode/.local/bin" $PATH
