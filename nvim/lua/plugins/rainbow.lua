538e910cca1c5a083d1c60ee250a4d8b02663070
Yohany Flores <yohanyflores@gmail.com>
2026-06-21 18:07:13 -0500

    qUITANDO CONFIG

--- nvim/lua/plugins/rainbow.lua
+++ nvim/lua/plugins/rainbow.lua
@@ -3,10 +3,5 @@ return {
         "HiPhish/rainbow-delimiters.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            -- The plugin works automatically out of the box by hooking into Treesitter.
-            -- No complex manual setup is needed, it will use the theme's colors
-            -- to color parentheses, brackets, and braces.
-        end,
     }
 }
