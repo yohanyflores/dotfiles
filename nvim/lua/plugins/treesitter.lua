7ca07937b10a22af7abed69b970b6d4c57cedb22
Yohany Flores <yohanyflores@gmail.com>
2026-06-21 19:33:15 -0500

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,75 @@
-return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        main = "nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "bash",
-                "html",
-                "css",
-                "python",
-                "javascript",
-                "typescript",
-                "go",
-                "java",
-                "clojure",
-                "sql",
-                "markdown",
-                "markdown_inline",
+local has_modern_nvim = vim.fn.has("nvim-0.12.0") == 1
+
+if has_modern_nvim then
+    -- Configuración moderna para Neovim >= 0.12.0 (Host)
+    return {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            main = "nvim-treesitter",
+            opts = {
+                ensure_installed = {
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "bash",
+                    "html",
+                    "css",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "go",
+                    "java",
+                    "clojure",
+                    "sql",
+                    "markdown",
+                    "markdown_inline",
+                },
+                sync_install = false,
+                highlight = { 
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = { enable = true },
             },
-            sync_install = false,
-            highlight = { 
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-        },
+        }
     }
-}
+else
+    -- Configuración compatible (Legacy) para Neovim < 0.12.0 (Contenedor)
+    return {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            branch = "master", -- Forzar la rama antigua compatible con Neovim < 0.12
+            build = ":TSUpdate",
+            config = function()
+                local configs = require("nvim-treesitter.configs")
+                configs.setup({
+                    ensure_installed = {
+                        "lua",
+                        "vim",
+                        "vimdoc",
+                        "query",
+                        "bash",
+                        "html",
+                        "css",
+                        "python",
+                        "javascript",
+                        "typescript",
+                        "go",
+                        "java",
+                        "clojure",
+                        "sql",
+                        "markdown",
+                        "markdown_inline",
+                    },
+                    sync_install = false,
+                    highlight = { 
+                        enable = true,
+                    },
+                    indent = { enable = true },
+                })
+            end,
+        }
+    }
+end
