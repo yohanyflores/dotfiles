211f9927a987d374ebf719032ad1d70fd83ba750
Yohany Flores <yohanyflores@gmail.com>
2026-06-21 21:23:36 -0500

    configurando

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -17,6 +17,9 @@ vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
+vim.opt.laststatus = 3   -- Barra de estado global única al final de la pantalla
+vim.opt.showmode = false   -- Ocultar el modo de texto duplicado abajo
+vim.opt.ruler = false      -- Desactivar el ruler tradicional para evitar la barra extra inferior
 
 -- Clipboard integration (system clipboard integration)
 vim.opt.clipboard = "unnamedplus"
